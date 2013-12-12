5a0dd9c199011aec66b98ed8bcc2d2145b2aaf2c
Chris Stillson <stillson@gmail.com>
2013-12-12 13:20:50 -0800

    bumped to 2.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup, Extension
 
 setup(name='rdrand',
-      version='0.2.2',
+      version='0.2.3',
       description="Python interface to Intel hardware rng",
       long_description= \
       """python random.Random() subclass which uses a c
